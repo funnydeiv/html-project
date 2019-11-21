f2e896ca6207c5f50ada88533d7528e85976d9ff
funnydeiv <funnydeiv@gmail.com>
2019-11-21 18:04:57 +0000

    ss

--- js/index.js
+++ js/index.js
@@ -2,17 +2,19 @@ document.querySelector('.rate-form input[type=submit]')
     .addEventListener('click', rate);
 
 function rate(e) {
-    e.preventDefault();
-    fetch('rate', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            name: document.querySelector('.rate-form input[name=name]').value,
-            email: document.querySelector('.rate-form input[name=email]').value,
-            message: document.querySelector('.rate-form textarea[name=message]').value
+    if (form.checkValidity()) {
+        e.preventDefault();
+        fetch('rate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: document.querySelector('.rate-form input[name=name]').value,
+                email: document.querySelector('.rate-form input[name=email]').value,
+                message: document.querySelector('.rate-form textarea[name=message]').value
+            })
         })
-    })
-        .then(_ => document.querySelector('.rate-form').reset());
+            .then(_ => document.querySelector('.rate-form').reset());
+    }
 }
