a7ff6807be64c403185bc1f5c11a649d0b9ea2cd
funnydeiv <funnydeiv@gmail.com>
2019-11-21 21:21:57 +0000

    end

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.querySelector('.rate-form input[type=submit]')
     .addEventListener('click', rate);
 
 function rate(e) {
-    if (form.checkValidity()) {
+    if (document.querySelector('.rate-form').checkValidity()) {
         e.preventDefault();
         fetch('rate', {
             method: 'POST',
@@ -12,9 +12,11 @@ function rate(e) {
             body: JSON.stringify({
                 name: document.querySelector('.rate-form input[name=name]').value,
                 email: document.querySelector('.rate-form input[name=email]').value,
-                message: document.querySelector('.rate-form textarea[name=message]').value
+                message: document.querySelector('.rate-form textarea[name=message]').value,
+                rate: ((document.querySelector('.rate-form input[name=rateRadio]:checked').id)[4] + '★')
             })
         })
             .then(_ => document.querySelector('.rate-form').reset());
     }
 }
+
